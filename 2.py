7c94389cd1c73227db6fecfec449e3829ba42eaf
Srikrishna1905 <srikrishnapaul95.com>
2026-06-21 18:43:13 +0530

    Has anyone exist who understand me better than myself.

---
+++ 2.py
@@ -0,0 +1,4 @@
+print("I'm looking for a job in the field of data science, machine learning, "
+"or artificial intelligence. I have experience with Python, R, and SQL, and " 
+"I'm eager to apply my skills to real-world problems. If you "
+"know of any opportunities, please let me know!")
